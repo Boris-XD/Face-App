b6a6f7af61ca5061fd5200cc5957a9e4c8da601c
Boris Sea <seaborisrc@gmail.com>
2023-11-18 19:37:03 -0400

    FaceApp/Container - Add values to the Border

--- lib/widgets/container.dart
+++ lib/widgets/container.dart
@@ -9,10 +9,20 @@ class MyContainer extends StatelessWidget {
       style: TextStyle(),
       child: Center(
         child: Container(
-          color: Colors.white,
           width: 200,
           height: 200,
           alignment: Alignment.center,
+          padding: EdgeInsets.symmetric(
+            horizontal: 20,
+            vertical: 20
+          ),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.horizontal(
+                left: Radius.circular(20),
+                right: Radius.circular(60)
+              ),
+          ),
           child: Text(
             "Boris Sea",
             style: TextStyle(
