a01437eb47494c9d59e8faab40654b01de3c65c1
Boris Sea <seaborisrc@gmail.com>
2024-11-21 13:26:34 -0400

    Drawer - Add drawer

--- lib/widgets/scafolds/scafold_one.dart
+++ lib/widgets/scafolds/scafold_one.dart
@@ -5,6 +5,26 @@ class MyScafoldOne extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: const Drawer(
+        elevation: 10,
+        child: Column(
+          children: [
+            Text("Boris@test.com"),
+          ],
+        ),
+      ),
+      drawerEnableOpenDragGesture: false,
+      endDrawerEnableOpenDragGesture: false,
+      onDrawerChanged: (isOpened) => print(isOpened),
+      onEndDrawerChanged: (isOpened) => print(isOpened),
+      endDrawer: const Drawer(
+        elevation: 10,
+        child: Column(
+          children: [
+            Text("Manu@test.com"),
+          ],
+        ),
+      ),
       appBar: AppBar(
         systemOverlayStyle: const SystemUiOverlayStyle(
           statusBarBrightness: Brightness.dark,
@@ -13,11 +33,11 @@ class MyScafoldOne extends StatelessWidget {
         elevation: 10,
         shadowColor: Colors.blue,
         titleTextStyle: const TextStyle(
-          color: Colors.black,
+          color: Colors.white,
           fontSize: 18,
         ),
-        title: Row(
-          children: const [
+        title: const Row(
+          children: [
             Text("Flutter  "),
             Text(
               "App",
