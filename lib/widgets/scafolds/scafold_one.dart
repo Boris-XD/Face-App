51b87e2ce8dbdfb8aa87634de09c7084edb7480a
Boris Sea <seaborisrc@gmail.com>
2024-11-22 21:54:18 -0400

    Scafold - Add first scafold

--- lib/widgets/scafolds/scafold_one.dart
+++ lib/widgets/scafolds/scafold_one.dart
@@ -2,9 +2,30 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 class MyScafoldOne extends StatelessWidget {
+  const MyScafoldOne({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.arrow_back),
+        onPressed: () {
+          print("Hello Manu");
+        },
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: "Profile",
+          )
+        ],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
       drawer: const Drawer(
         elevation: 10,
         child: Column(
@@ -17,19 +38,29 @@ class MyScafoldOne extends StatelessWidget {
       endDrawerEnableOpenDragGesture: false,
       onDrawerChanged: (isOpened) => print(isOpened),
       onEndDrawerChanged: (isOpened) => print(isOpened),
-      endDrawer: const Drawer(
+      /*endDrawer: const Drawer(
         elevation: 10,
         child: Column(
           children: [
             Text("Manu@test.com"),
           ],
         ),
-      ),
+      ),*/
       appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.settings),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.logout),
+          )
+        ],
         systemOverlayStyle: const SystemUiOverlayStyle(
           statusBarBrightness: Brightness.dark,
         ),
-        backgroundColor: const Color(0xff01579b),
+        backgroundColor: const Color.fromARGB(255, 46, 50, 53),
         elevation: 10,
         shadowColor: Colors.blue,
         titleTextStyle: const TextStyle(
@@ -46,6 +77,24 @@ class MyScafoldOne extends StatelessWidget {
                 color: Colors.white,
               ),
             )
+          ],
+        ),
+      ),
+      body: Container(
+        color: Colors.grey,
+        width: double.infinity,
+        height: double.infinity,
+        child: Column(
+          children: [
+            Expanded(
+                child: const Center(
+              child: Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 30,
+                  ),
+                  child: TextField()),
+            )),
+            Text("Manu")
           ],
         ),
       ),
